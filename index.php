0300e059c1c1a4f74c9da17592dc4f82048c33d4
cheshire96 <belugarciaarcija@gmail.com>
2017-09-08 00:24:07 -0300

    practicaII

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Practica WebII</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/dataTables.bootstrap.css" rel="stylesheet">
+
+    
+  </head>
+  <body>
+  
+    <div class="container">
+	    <div class="panel panel-default">
+	      <div class="panel-body">
+
+			<a href="class/prueba.php" class="btn btn-info btn-md"><span class="glyphicon glyphicon-pen" aria-hidden="true"></span> Formulario</a>
+	
+	      </div>
+	    </div>
+	    
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.dataTables.min.js"></script>
+    <script src="js/dataTables.bootstrap.js"></script>
+    <script type="text/javascript" charset="utf-8">
+	$(document).ready(function() {
+	  $('#ghatable').dataTable();
+	});
+    </script>
+  </body>
+</html> 
