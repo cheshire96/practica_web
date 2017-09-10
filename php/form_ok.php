aff096383f33d86d68bec95e1f29ee3e5b0338b9
Macarena Belén Garcia Arcija <belugarciaarcija@gmail.com>
2017-09-10 01:27:16 -0300

    PracticaIII

--- php/form_ok.php
+++ php/form_ok.php
@@ -3,8 +3,10 @@
 include "header.php";
 ?>
 
-<p>WIIIIII CARGO</p>
+<h2><span class="glyphicon glyphicon-saved" aria-hidden="true">  Los datos se cargaron exitosamente</h2>
 
+<a href="../index.php" class="btn btn-link btn-md"><span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span> Volver</a>
+			
 	<?php
 		include "footer.php";
 		?>
